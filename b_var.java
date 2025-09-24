24600ddcaff291bd9358373dbbe3164cb87dc045
ghatraxion <agh.arrafif@gmail.com>
2025-09-24 11:09:23 +0700

    learning java

---
+++ b_var.java
@@ -0,0 +1,20 @@
+public class b_var {
+    public static void main(String[] args) {
+       int number = 10;
+       String text = "Hello, java";
+       char letter = 'A';
+       double decimal = 3.14;
+       boolean istrue = true;
+       boolean isfalse = false;
+
+       System.out.printf("Ini adalah variable integer: ", number);
+       System.out.printf("Ini adalah variable string: ", text);
+       System.out.printf("Ini adalah variable char: ", letter);
+       System.out.printf("Ini adalah variable double: ", decimal);
+       System.out.printf("Ini adalah variable Boolean true: ", istrue);
+       System.out.printf("Ini adalah variable Boolean False: ", isfalse);
+
+
+
+    }
+}

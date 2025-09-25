36d06efaf09041903717551b9ae2a9d4d3e3a1ff
ghatraxion <agh.arrafif@gmail.com>
2025-09-25 09:25:17 +0700

    java

---
+++ basic_fundamental/f_array.java
@@ -0,0 +1,8 @@
+public class f_array {
+    public static void main(String[] args) {
+        int[] number = {10, 20, 30 ,40, 50};
+        for (int i = 0; i < number.length; i++){
+            System.out.println(i);
+        }
+    }
+}

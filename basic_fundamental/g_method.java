39a2bfb12f82387f0e2d79093981292573a61ec4
ghatraxion <agh.arrafif@gmail.com>
2025-09-25 09:30:36 +0700

    java

---
+++ basic_fundamental/g_method.java
@@ -0,0 +1,11 @@
+public class g_method {
+    static int tambah(int x, int y) {
+        return x + y;
+    }
+    public static void main(String[] args) {
+        int result = tambah(5, 10);
+        System.out.println("Hasil: " + result);
+    }
+}
+//   INI METHOD
+//   METHOD ADALAH SEBUAH FUNGSI YANG DIGUNAKAN UNTUK MEMPERMUDAH KITA DALAM MENGGUNAKAN KODE YANG SAMA BERULANG KALI

e3f43ae62f55881b5a65dcccf479583936a4d058
ghatraxion <agh.arrafif@gmail.com>
2025-09-25 09:22:26 +0700

    java

---
+++ basic_fundamental/e_for_loop.java
@@ -0,0 +1,12 @@
+public class e_for_loop {
+    public static void main(String[] args) {
+        for (int i = 1; i <= 5; i++) {
+            System.out.println("*" + i);
+        }
+        int j = 1;
+        while (j <= 5) {
+            System.out.println("#" + j);
+            j++;
+        }
+    }
+}

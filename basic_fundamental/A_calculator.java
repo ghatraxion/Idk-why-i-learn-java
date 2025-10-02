83e233611fb1b41425a3610e45e2edf28d5e3497
ghatraxion <agh.arrafif@gmail.com>
2025-10-02 10:24:27 +0700

    dart

---
+++ basic_fundamental/A_calculator.java
@@ -0,0 +1,11 @@
+public class A_calculator {
+    public static void main(String[] args) {
+        System.out.println("=== PROGRAM KALKULATOR V 1.0 ===\n");
+        System.out.println("Pilih metode mtk: ");
+        System.out.println("1. Penjumlahan");
+        System.out.println("2. Pengurangan");
+        System.out.println("3. Perkalian");
+        System.out.println("4. Pembagian");
+        // belum selesai, lagi di kelas soalnya
+    }
+}

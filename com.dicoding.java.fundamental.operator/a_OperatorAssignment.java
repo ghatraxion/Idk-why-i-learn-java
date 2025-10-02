d0d66c194bc51fe114df1c37e874201830669676
ghatraxion <agh.arrafif@gmail.com>
2025-10-02 20:49:52 +0700

    java

---
+++ com.dicoding.java.fundamental.operator/a_OperatorAssignment.java
@@ -0,0 +1,9 @@
+package com.dicoding.java.fundamental.operator;
+public class a_OperatorAssignment {
+    public static void main(String[] args) {
+     int firstnum = 2;
+     int secondnum = 19;
+     System.out.println("data pada firstnum adalah: " + firstnum);
+     System.out.println("data pada secondnum adalah: " + secondnum);
+    }
+}
